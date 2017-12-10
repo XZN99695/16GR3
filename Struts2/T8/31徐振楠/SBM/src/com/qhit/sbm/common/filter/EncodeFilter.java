cfa87d2097f5087f2204f45e447b6810194cb5fa
XZN99695 <33885200+XZN99695@users.noreply.github.com>
2017-12-10 23:27:48 +0800

    31徐振楠提交StrutsT8作业

---
+++ Struts2/T8/31徐振楠/SBM/src/com/qhit/sbm/common/filter/EncodeFilter.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.qhit.sbm.common.filter;
+
+import java.io.IOException;
+
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class EncodeFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		System.out.println("缂栫爜鏍煎紡杩囨护鍣ㄩ攢姣�);
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1,
+			FilterChain arg2) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) arg0;
+		HttpServletResponse response = (HttpServletResponse) arg1;
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		arg2.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		System.out.println("缂栫爜鏍煎紡杩囨护鍣ㄥ垵濮嬪寲");
+	}
+
+}
